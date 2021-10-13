cdbb0b8d4412e8f70ace0b0082f954422af383f1
MiLunaYunorvi <misusuyatec@gmail.com>
2021-10-13 11:39:31 -0500

    compara dni en db para usuarios

--- EXCAP/modulos/Portalweb/views.py
+++ EXCAP/modulos/Portalweb/views.py
@@ -6,7 +6,17 @@ from datetime import datetime
 # Create your views here.
 url=''
 mac=''
+dni=''
+base_url=''
 
+def consulta_usuario_new(dni_v):
+    if Usuario.objects.filter(dni=dni_v):
+        print('va al BCP')
+        ulr_con='https://www.viabcp.com/'
+    else:
+        print('va al bbva, nuevo')
+        ulr_con='www.bbva.pe'
+    return ulr_con
 
 
 def lista(request):
@@ -15,26 +25,29 @@ def lista(request):
     return render(request,'usuarios.html',{"usuarios":usuariosenlista})
 
 def formulario(request):
-    global url,mac
-    user_url=request.GET['user_continue_url']
+    global mac,base_url
     base_url=request.GET['base_grant_url']
     mac=request.GET['client_mac']
-    url = base_url + '?continue_url=' + user_url + '&duration=120'
-    print(url)
-    print('url',request.GET['user_continue_url'],)
     return render (request, 'formulario.html')
 
 def registrar(request):
-    global mac
+    global mac,dni,continue_url
     tiempo=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     nombre=request.POST['nombres']
     apellidos=request.POST['apellidos']
     dni=request.POST['dni']
     email=request.POST['email']
     print(datetime)
+    user_url=consulta_usuario_new(dni)
     usuario= Usuario.objects.create(nombre=nombre, apellidos=apellidos, dni=dni, email=email,mac=mac,tiempo=str(tiempo))
     return redirect('/acceder')
 
 def acceder(request):
-    global url
+    global url,mac,dni,base_url
+    #user_url=request.GET['user_continue_url']
+    print('BASE_URL: ',base_url,'\n','DNI: ',dni)
+    
+    url = base_url + '?continue_url=' + user_url + '&duration=120'
+    print(url)
+    #print('url',request.GET['user_continue_url'],)
     return render(request,'acceder.html',{'url_get':url})
